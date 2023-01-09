c6e1b3c34157ba4ac1b64d82eec6653c50fc4f54
Rowan <60217814+RTrickett@users.noreply.github.com>
2023-01-09 14:37:54 +0000

    Update main.py

--- main.py
+++ main.py
@@ -8,11 +8,11 @@ import dash_bootstrap_components as dbc
 from dash_bootstrap_templates import load_figure_template
 from dash.dependencies import Input, Output
 
+# Load dash stylesheet
 load_figure_template("darkly")
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.DARKLY])
 
-# Import and clean third vaccination uptake by % data
-# df = pd.read_csv("vaccination-data.csv", usecols=['ISO3', 'COUNTRY', 'DATE_UPDATED', 'PERSONS_FULLY_VACCINATED_PER100'])
+# Import data
 df = pd.read_csv("Covid-Poverty_data.csv")
 print(df[0:5])
 
@@ -20,7 +20,7 @@ print(df[0:5])
 # ------------------------------------------------------------------------------
 # Connect the Plotly graphs with Dash Components
 
-# Plotly Express
+# Plotly Express choropleth map
 fig = px.choropleth(
     data_frame=df,
     locations='ISO3',
@@ -35,7 +35,6 @@ fig = px.choropleth(
 )
 
 fig.update_layout(
-    #title_text="Persons Fully Vaccinated by Country (%)",
     title_xanchor="center",
     title_font=dict(size=22),
     title_x=0.5,
@@ -45,13 +44,9 @@ fig.update_layout(
 )
 
 fig.update_coloraxes(showscale=True, colorbar_orientation='h')
-#fig.update_layout(template='plotly_dark')
 fig.update_layout(geo=dict(bgcolor='rgba(0,0,0,0)'), coloraxis_colorbar_y=0, coloraxis_colorbar=dict(titleside='top'))  # remove background
 
-
-# ------------------------------------------------------------------------------
-# Connect the Plotly graphs with Dash Components
-
+# Scatter plot comparing covid deaths per million and gdp per capita
 fig2 = px.scatter(df,
                   x='Deaths_per_million',
                   y='GDP_per_capita',
@@ -59,11 +54,11 @@ fig2 = px.scatter(df,
                   title='The Effect of GDP on Covid Deaths',
                   labels={'Deaths_per_million': 'Deaths per million',
                           'GDP_per_capita': 'GDP per capita'},
-                  trendline="ols")
-#fig2.update_layout(template='plotly_dark', title_x=0.5)
+                  trendline="ols") # inlcude trendline using ordinary least squares 
 fig2.update_layout(title_x=0.5)
 fig2.update_layout(margin=dict(t=50, r=20, l=25, b=20))
 
+# Scatter plot comparing the number of people vaccinated per 100 and gdp per capita
 fig3 = px.scatter(df,
                   x='Persons_fully_vaccinated_per100',
                   y='GDP_per_capita',
@@ -72,63 +67,13 @@ fig3 = px.scatter(df,
                   labels={'Persons_fully_vaccinated_per100': 'Persons Fully Vaccinated per 100',
                           'GDP_per_capita': 'GDP per capita'},
                   trendline="ols")
-#fig3.update_layout(template='plotly_dark', title_x=0.5)
 fig3.update_layout(title_x=0.5)
 fig3.update_layout(margin=dict(t=50, r=20, l=25, b=20))
 
-# ------------------------------------------------------------------------------
-# App layout - Original
-
-# app.layout = dbc.Container([
-#
-#     dbc.Row([
-#         dbc.Col([
-#             html.H1("A Comparison of Covid Deaths with Vaccination and Poverty Rates")
-#         ], width=10)
-#     ], justify="center", align='center'),
-#
-#     dbc.Col([
-#         dbc.Card([
-#             dcc.Dropdown(id='my_dropdown',
-#                         options=[
-#                             {'label': 'Population Fully Vaccinated (%)', 'value': 'Persons_fully_vaccinated_per100'},
-#                             {'label': 'Covid deaths (per million)', 'value': 'Deaths_per_million'},
-#                             {'label': 'Population Below Poverty Line (%)', 'value': 'Population_below_poverty_line'},
-#                             {'label': 'GDP per Capita', 'value': 'GDP_per_capita'}
-#                         ],
-#                         value='Persons_fully_vaccinated_per100',
-#                         multi=False,    # Disable multiple selection
-#                         searchable=False,
-#                         style={'text-align': 'center', 'color': 'black', 'white-space': 'nowrap'}
-#                         ),
-#             dcc.Graph(id='my_vaccine_map', figure=fig, className="h-100", style={'margin': 5})
-#         ], style={'border-radius': '2%', 'align': 'center', 'height': '50vh'}),
-#     ]),
-#
-#     html.Br(),
-#
-#     dbc.Row([
-#         dbc.Col([
-#             dbc.Card([
-#                 #html.H5("The Effect of GDP on Covid Deaths", className="card-title", style={'textAlign': 'center'}),
-#                 dcc.Graph(id='Scatter_plot', figure=fig2, className="h-100", style={'margin': 2})
-#             ], style={'border-radius': '2%', 'align': 'center', 'height': '35vh'}),
-#         ], width=6),
-#
-#         dbc.Col([
-#             dbc.Card([
-#                 #html.H5("The Effect of GDP on Covid Vaccinations", className="card-title", style={'textAlign': 'center'}),
-#                 dcc.Graph(id='Scatter_plot', figure=fig3, className="h-100", style={'margin': 2})
-#             ], style={'border-radius': '2%', 'align': 'center', 'height': '35vh'})
-#         ], width=6),
-#     ]),
-#
-#     html.Br()
-#
-# ], fluid=True)
 
 # ------------------------------------------------------------------------------
 # App layout - New
+# Using Dash bootstrap components
 
 app.layout = dbc.Container([
 
@@ -160,7 +105,6 @@ app.layout = dbc.Container([
         dbc.Col([
             dbc.Row([
                 dbc.Card([
-                    #html.H5("The Effect of GDP on Covid Deaths", className="card-title", style={'textAlign': 'center'}),
                     dcc.Graph(id='Scatter_plot', figure=fig2, className="h-100", style={'margin': 2})
                 ], style={'border-radius': '2%', 'align': 'center', 'height': '44vh'}),
             ], style={'margin-right': '1%'}),
@@ -169,7 +113,6 @@ app.layout = dbc.Container([
 
             dbc.Row([
                 dbc.Card([
-                    #html.H5("The Effect of GDP on Covid Vaccinations", className="card-title", style={'textAlign': 'center'}),
                     dcc.Graph(id='Scatter_plot', figure=fig3, className="h-100", style={'margin': 2})
                 ], style={'border-radius': '2%', 'align': 'center', 'height': '44vh'})
             ], style={'margin-right': '1%'})
@@ -191,7 +134,7 @@ app.layout = dbc.Container([
 
 ], fluid=True)
 
-
+# Update map with data selected via the dropdown
 @app.callback(Output('my_vaccine_map', 'figure'),
               Input('my_dropdown', 'value')
 )
@@ -216,6 +159,7 @@ def update_graph(value):
         geo=dict(scope='europe')  # what section of the world is being shown
     )
 
+    # Remove map background colour and change colourbar position
     fig.update_layout(geo=dict(bgcolor='rgba(0,0,0,0)'), coloraxis_colorbar_y=0, coloraxis_colorbar=dict(titleside='top'))
     fig.update_coloraxes(showscale=True, colorbar_orientation='h')
 
